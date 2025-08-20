136a530507759016d4d81bb26d2d0020bfcf6548
Son Nguyen <son.nguyen.xg@bp.renesas.com>
2025-08-20 11:14:09 +0700

    add renesas/rz/board/rzg2l_100
    Signed-off-by: Son Nguyen <son.nguyen.xg@bp.renesas.com>

--- plat/renesas/rz/board/rzg2l_100/rz_board.mk
+++ plat/renesas/rz/board/rzg2l_100/rz_board.mk
@@ -4,8 +4,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
-DDR_SOURCES +=  plat/renesas/rz/soc/${PLAT}/drivers/ddr/param_mc_g2l.c \
-				plat/renesas/rz/common/drivers/ddr/param_swizzle_g2l.c
+DDR_SOURCES +=  plat/renesas/rz/soc/${PLAT}/drivers/ddr/ddr_g2l.c \
+				plat/renesas/rz/board/rzg2l_100/param_mc_C-010_D4-01-1.c \
+				plat/renesas/rz/board/rzg2l_100/param_swizzle_T1c.c
 
 DDR_PLL4    := 1600
 $(eval $(call add_define,DDR_PLL4))
